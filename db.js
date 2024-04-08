e89649d43db68aa7b06cf76932973e4e2b82e6f6
adrian-coronel <adrianonel78@gmail.com>
2024-04-08 12:20:23 -0500

    dfsfd

--- db.js
+++ db.js
@@ -1,6 +1,4 @@
-// Importar la biblioteca Mongoose
-const mongoose = require('mongoose');
-
+const mysql = require('mysql')
 // Desestructurar las variables de entorno relacionadas con la conexión a MongoDB
 const {
   MONGO_USERNAME,
@@ -10,22 +8,11 @@ const {
   MONGO_DB
 } = process.env;
 
-// Configuración de opciones para la conexión a MongoDB
-const options = {
-  useNewUrlParser: true,                    // Indica a Mongoose que utilice el nuevo analizador de URL
-  reconnectTries: Number.MAX_VALUE,         // Intenta reconectar de forma indefinida
-  reconnectInterval: 500,                    // Intervalo entre intentos de reconexión (en milisegundos)
-  connectTimeoutMS: 10000,                   // Tiempo máximo de espera para establecer una conexión (en milisegundos)
-};
-
-// Construcción de la URL de conexión a MongoDB
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-
-// Intento de conexión a la base de datos MongoDB utilizando Mongoose
-mongoose.connect(url, options)
-  .then(() => {
-    console.log('MongoDB is connected');     // Mensaje de éxito cuando la conexión se realiza correctamente
-  })
-  .catch(err => {
-    console.log(err);                        // Manejo de errores si la conexión falla
-  });
+var connection = mysql.createConnection(
+    {
+        host: MONGO_HOSTNAME, 
+		user: MONGO_USERNAME,  
+		password: MONGO_PASSWORD, 
+		database: MONGO_DB,
+    }
+)
