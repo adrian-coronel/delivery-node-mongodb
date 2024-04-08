fb7e2b20b22380f28b7a291e2d16fa68b7e24c9f
adrian-coronel <adrianonel78@gmail.com>
2024-04-08 10:52:36 -0500

    first

--- db.js
+++ db.js
@@ -1,11 +1,31 @@
+// Importar la biblioteca Mongoose
 const mongoose = require('mongoose');
 
-const MONGO_USERNAME = 'userdemo';
-const MONGO_PASSWORD = 'Tecsup';
-const MONGO_HOSTNAME = '127.0.0.1';
-const MONGO_PORT = '27017';
-const MONGO_DB = 'deliverydb';
+// Desestructurar las variables de entorno relacionadas con la conexión a MongoDB
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB
+} = process.env;
 
+// Configuración de opciones para la conexión a MongoDB
+const options = {
+  useNewUrlParser: true,                    // Indica a Mongoose que utilice el nuevo analizador de URL
+  reconnectTries: Number.MAX_VALUE,         // Intenta reconectar de forma indefinida
+  reconnectInterval: 500,                    // Intervalo entre intentos de reconexión (en milisegundos)
+  connectTimeoutMS: 10000,                   // Tiempo máximo de espera para establecer una conexión (en milisegundos)
+};
+
+// Construcción de la URL de conexión a MongoDB
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
-mongoose.connect(url, {useUnifiedTopology: true, useNewUrlParser: true});
+// Intento de conexión a la base de datos MongoDB utilizando Mongoose
+mongoose.connect(url, options)
+  .then(() => {
+    console.log('MongoDB is connected');     // Mensaje de éxito cuando la conexión se realiza correctamente
+  })
+  .catch(err => {
+    console.log(err);                        // Manejo de errores si la conexión falla
+  });
